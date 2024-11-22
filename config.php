1db5197c9c7ae462cb6529eb77d41b4cf69e686a
josels7777777 <constructionjlma@gmail.com>
2024-11-21 21:01:24 -0800

    UID session
    Mod for create character and deleted some extra spaces on config.php

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
-// config.php
-
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', ''); // Default password for XAMPP is an empty string
 define('DB_NAME', 'rpg');
+?>
